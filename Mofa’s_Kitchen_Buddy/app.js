4cb6cc6500cad04238f7abeca6f44c3c543f7b8c
DurjoyGH <durjoyghosh328@gmail.com>
2024-12-21 21:09:20 +0600

    added functionality

--- Mofa’s_Kitchen_Buddy/app.js
+++ Mofa’s_Kitchen_Buddy/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const ingredientRoutes = require('./routes/ingredientRoutes');
+const recipesRouter = require("./routes/recipesRouter");
 
 dotenv.config();
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.DB_CONNECTION)
 
 // Routes
 app.use('/api/ingredients', ingredientRoutes);
+app.use('/api/recipes', recipesRouter);
 
 // Start server
 app.listen(PORT, () => {
